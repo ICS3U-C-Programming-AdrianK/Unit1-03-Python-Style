aeb1a2581a4549b920d35e7b6b88d3c7b3c66f45
Adrian kouakam <adrian.kouakam-guemiebing@stu.ocsb.ca>
2026-09-15 02:26:16 +0000

    First commit of hello_world_updated_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# # Created By: Adrian Student
+# Date: sep 14th, 2026
+# This program displays a greeting and contains proper formatting.
+def main():
+    print("Hello, adrian")
+    print("i love school")
+
+
+if __name__ == "__main__":
+    main()
